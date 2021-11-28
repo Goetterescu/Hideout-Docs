2df80a5c259d5464fdbc081d12b0e46c121beab9
Goetterescu <daniel.goetter@gmx.de>
2021-11-28 19:10:32 +0100

    Converted txt files to HTML Added new rules Edited rules Changed JS code so it displays additional info and shows the HTML in iframe Documented JS code Added styling for rules page

--- rules/main.js
+++ rules/main.js
@@ -1,4 +1,6 @@
-let work; // This boolean allows info about rules to be displayed as HTML content
+let work;       // This boolean allows info about rules to be displayed as HTML content
+let clear_it;   // This boolean determines if it's needed to clear the info display
+let empty_msg = "There is no information for this article"; // Message for empty info display
 
 function check_workers() {
     // See if Web Workers is working
@@ -12,19 +14,43 @@ function check_workers() {
 
 
 
-function show(com) {
+function run(com) {
+    clear_it = true; // Let's activate the clearing thingy, because otherwise I have to do it in every case where I don't want to clear the info display
     // Check if Web Workers is working
     if (work = true) {
         switch (com) { // Now just check what info should be displayed and do so
             case "general_rp":
-                document.getElementById("more_info").innerHTML = "This content might be interesting for you: <a target='_blank' href='https://forum.rigsofrods.org/resources/small-car-trailer.382/'>Camera Trailer</a>, <a target='_blank' href='https://forum.rigsofrods.org/resources/1999-ford-crown-victoria.147/'>Ford</a>, <a target='_blank' href='https://forum.rigsofrods.org/resources/gavril-omega-det.591/'>Gavril</a>";
+                window.open('general_rp.html', 'rule_display');
             break;
-            default: // Clear info when none present
-                document.getElementById("more_info").innerHTML = "There is no information for this article";
-                console.error("Switch failed in function show. Unknown info-type: " + com + ". This is no Issue!");
+            case "vcg":
+                window.open('VCG.html', 'rule_display');
+                document.getElementById("more_info").innerHTML = "We will show some example images here soon!";
+                clear_it = false; 
+            break;
+            case "btl":
+                window.open('BTL.html', 'rule_display');
+            break;
+            case "chr":
+                window.open('CHR.html', 'rule_display');
+            break;
+            case "discord":
+                window.open('discord.html', 'rule_display');
+            break;
+            case "game":
+                window.open('game.html', 'rule_display');
+            break;
+            default: // Output error message when there is no match (info type). This can happend when I'm lazy
+                console.error("Switch failed. Info-type: " + com + ". This is no Issue!");
+                clear_it = false;
             break;
         }
-        console.log("Info-type: " + com);
+        console.log("Info-type: " + com); // Log the info type
+
+        if (clear_it == true) {
+            // Clear info when none present
+            document.getElementById("more_info").innerHTML = empty_msg;
+        }
+
     } else {
         document.getElementById("more_info").innerHTML = "Web Workers not working on your browser/device!";
     }
