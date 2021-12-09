963136f7a01c4a9cc0799b6b46bf8906c71b01c4
Goetterescu <daniel.goetter@gmx.de>
2021-12-09 23:46:01 +0100

    Added ToS and general edit Edited the JS code of the rules index (made it smaller and better)

--- rules/main.js
+++ rules/main.js
@@ -12,28 +12,17 @@ function check_workers() {
 
 
 function run(com) {
-    showi("clear"); // Clear the info box 
-
     // We are using this great function here for something really cool
     if (com == "VCG" || com == "economy") { // Add rules with information here
         window.open(com + '.html', 'rule_display');
-        showi(com); // This way we can say that we want to show additional info
-
-    } else if (com == "general_rp" || com == "BTL" || com == "CHR" || com == "discord" || com == "game") { // Add rules which have no info
+        window.open('info/' + com + '.html', 'info_box'); // Show additional info
+    } else if (com == "general_rp" || com == "BTL" || com == "CHR" || com == "discord" || com == "game" || com == "tos") { // Add rules which have no info
         window.open(com + '.html', 'rule_display');
+        window.open('info/clear.html', 'info_box'); // Clear the info box
     } else {
         // Output error message when there is no match (info type). This can happend when I'm lazy
         console.error("Switch failed. Info-type: " + com + ". This is no Issue!");
     } // Tada!!! This way we can easily add new rules and display them, with info and without!
+
     console.log("Info-type: " + com); // Log the info type
 }
-
-function showi(x) { // Show information
-    if (x == "clear") {
-        window.open('info/clear.html', 'info_box');
-    } else if (x == "VCG" || x == "economy") { // Just continue on this with an or (||) if you want to add more information for more rules. Yes, I am clever and my coding is flexible
-        window.open('info/' + x + '.html', 'info_box');
-    } else {
-        console.error("Error in if-statement of function 'showi'. It seems like there was no match for information...");
-    }
-} 
